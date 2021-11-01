7c9a53f302e43a2dad79a71c1fe768a89456000f
Kendrix0 <kevin.nguyen9917@gmail.com>
2021-11-01 12:32:56 -0500

    Added skeleton code

---
+++ app.js
@@ -0,0 +1,14 @@
+const displayController = (() => {
+
+})();
+
+const gameBoard = (() => {
+
+})();
+
+const player = (name, sign) => {
+    const getName = () => name;
+    const getSign = () => sign;
+
+    return {getName, getSign}
+};

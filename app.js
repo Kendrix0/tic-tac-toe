70b2cf19ed6347ec6d3169d13d24474e4de17d91
Kendrix0 <kevin.nguyen9917@gmail.com>
2021-11-01 14:41:31 -0500

    Adding some logic to modules and factory fxn

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
-const displayController = (() => {
+const displayController = (() => {    
+    const resetBtn = document.querySelector('.reset');
+    const spaces = document.querySelectorAll('.space');
 
-})();
+    spaces.forEach((space) => {
+        space.onclick = () => {
+            space.innerHTML = 'CLICKED!'
+        }
+    });
 
-const gameBoard = (() => {
+    const updateBoard = () => {
+
+    };
+
+    resetBtn.onclick = () => {
+        gameBoard.reset();
+    }
 
 })();
 
 const player = (name, sign) => {
-    const getName = () => name;
-    const getSign = () => sign;
-
+    this.name = name
+    this.sign = sign
+    
+    const getName = () => {
+        return name
+    }
+    const getSign = () => {
+        return sign
+    }
+    
     return {getName, getSign}
 };
+
+const gameBoard = (() => {
+    let board = ['X','O','X','O','X','O','X','O','X'];
+    
+    const setSpace = (space, sign) => {
+        board[space] = sign
+    }
+
+    const reset = () => {
+        board = ['','','','','','','','','']
+    };
+
+    return {setSpace, reset}
+})();

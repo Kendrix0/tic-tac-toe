69284e94ab0b25840af825d9461f23757faee60d
Kendrix0 <kevin.nguyen9917@gmail.com>
2021-11-03 15:18:11 -0500

    Added more styling and cleaned some logic

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const X = document.querySelector('#X');
 const O = document.querySelector('#O');
-const startMenu = document.getElementById('start')
+const startMenu = document.getElementById('start');
 X.onclick = () => {
     displayController.init('X', 'O');
-    startMenu.style.zIndex = '-1'
+    startMenu.style.display = 'none';
 }
 
 O.onclick = () => {
     displayController.init('O', 'X');
-    startMenu.style.zIndex = '-1'
+    startMenu.style.display = 'none';
 }
 
 const displayController = (() => {    
     const resetBtn = document.querySelector('.reset');
+    const startBtn = document.querySelector('.start')
     const spaces = document.querySelectorAll('.space');
+    const display = document.querySelector('.display');
+    let gameOn = true
     let turn = 0
 
     const init = (p1Sign, p2Sign) => {
@@ -42,25 +45,52 @@ const displayController = (() => {
 
     const declareWinner = (sign) => {
         if (p1.getSign() === sign) {
-            alert(`${p1.getName()} wins!`)
+            display.innerHTML = `${p1.getName()} wins!`
+        } else if (p2.getSign() === sign) {
+            display.innerHTML = `${p2.getName()} wins!`
         } else {
-            alert(`${p2.getName()} wins!`)
+            display.innerHTML = 'Tie game!'
         }
     }
 
     spaces.forEach((space) => {
         space.onclick = () => {
-            if (gameBoard.getSpace(space.id[1]) === '') {
-                gameBoard.setSpace(space.id[1], currentSign)
-                changePlayer()
-                updateBoard();
-                if (gameBoard.checkWinner()) {
-                    declareWinner(gameBoard.checkWinner());
-                    resetGame();
+            if (gameOn) {
+                if (gameBoard.getSpace(space.id[1]) === '') {
+                    gameBoard.setSpace(space.id[1], currentSign)
+                    changePlayer()
+                    updateBoard();
+                    if (checkWinner()) {
+                        declareWinner(checkWinner());
+                        resetGame();
+                        gameOn = false
+                    }
                 }
-            }
-        };
+            }    
+        }
     });
+
+    const checkWinner = () => {
+        const lines = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+        for (let i = 0; i < lines.length; i++) {
+            const [a,b,c] = lines[i];
+            if (gameBoard.getSpace([a]) && gameBoard.getSpace([a]) === gameBoard.getSpace([b]) && gameBoard.getSpace([a]) === gameBoard.getSpace([c])) {
+                return gameBoard.getSpace([a])
+            } else if (turn == 9) {
+                return 'tie'
+            }
+        }
+        return null
+    }
 
     const updateBoard = () => {
         for (i = 0; i < 9; i++) {
@@ -72,13 +102,20 @@ const displayController = (() => {
     const resetGame = () => {
         gameBoard.reset();
         currentPlayer = p1;
+        currentSign = p1.getSign();
         updateBoard();
         turn = 0;
     }
 
     resetBtn.onclick = () => {
         resetGame();
-        startMenu.style.zIndex = '1'
+        startMenu.style.zIndex = -1
+    }
+
+    startBtn.onclick = () => {
+        startMenu.style.display = 'flex';
+        display.innerHTML = 'Tic - Tac - Toe';
+        gameOn = true
     }
 
     return {init}
@@ -102,7 +139,6 @@ const gameBoard = (() => {
     let board = ['','','','','','','','',''];
     
     const setSpace = (space, sign) => {
-        checkWinner();
         board[space] = sign
     }
 
@@ -114,28 +150,9 @@ const gameBoard = (() => {
         board = ['','','','','','','','','']
     };
 
-    const checkWinner = () => {
-        const lines = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
-        ]
-        for (let i = 0; i < lines.length; i++) {
-            const [a,b,c] = lines[i];
-            if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-                return board[a]
-            }
-        }
-        return null
-    }
-    return { setSpace, getSpace, reset, checkWinner }
+    return { setSpace, getSpace, reset }
 })();
 
 
-//Add more styling
-//Once those are complete, create option to play against AI
+
+//create option to play against AI
